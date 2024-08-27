9242e9ccd9206e223577f31b35510ecc5b064a1a
Mohammad Iqbal <63330022+Iqbal0786@users.noreply.github.com>
2024-08-27 11:42:24 +0530

    designed home page and display static topcis to start a test

---
+++ frontend/data/question.ts
@@ -0,0 +1,39 @@
+export const  SAMPLE_TOPICS = [
+    {
+      name: 'Technology',
+      description: 'Test your knowledge on the latest technology trends.',
+      slug: 'technology',
+    },
+    {
+      name: 'Science',
+      description: 'Explore various scientific topics and questions.',
+      slug: 'science',
+    },
+    {
+      name: 'History',
+      description: 'Test your knowledge of historical events and figures.',
+      slug: 'history',
+    },
+    // Add more topics as needed
+  ];
+
+  export const MOCK_TEST = [
+    {
+      id: 1,
+      question: 'What is the capital of France?',
+      options: ['Paris', 'London', 'Berlin', 'Madrid'],
+      answer: 'Paris'
+    },
+    {
+      id: 2,
+      question: 'Which planet is known as the Red Planet?',
+      options: ['Earth', 'Mars', 'Jupiter', 'Saturn'],
+      answer: 'Mars'
+    },
+    {
+      id: 3,
+      question: 'Who wrote "To Kill a Mockingbird"?',
+      options: ['Harper Lee', 'J.K. Rowling', 'Ernest Hemingway', 'Mark Twain'],
+      answer: 'Harper Lee'
+    },
+  ]
