f854ffaf17a21bee72d763da04526f50a3a6a329
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-08-28 16:14:51 +0530

    implement step wise component rendering functionality

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,50 +1,86 @@
 "use client"
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactNode } from "react";
 import { SAMPLE_TOPICS } from "@/data/question";
 import Link from "next/link";
-
-function SkeletonPlaceholder() {
-  return (
-    <div className="block p-6 bg-gray-300 animate-pulse text-gray-300 rounded-lg shadow-lg">
-      <div className="w-[20rem] h-4 bg-gray-400 rounded mb-4"></div>
-      <div className="h-4 bg-gray-400 rounded mb-2"></div>
-      <div className="h-4 bg-gray-400 rounded"></div>
-    </div>
-  );
-}
+import RoleSelection from "@/components/ChoiceSelector";
+import Pagination from "@/components/Pagination";
+import { tree } from "next/dist/build/templates/app-page";
+import TopicList from "@/components/TopicList";
+import { useAppDispatch, useAppSelector } from "@/Redux/App/hooks";
+import { updateSelectedRole } from "@/Redux/Features/commonSlice";
+const Roles=[
+  {
+    id: 1,
+    title: "Frontend Developer",
+    description: "Start the quiz to test your UI/UX skills!"
+  },
+  {
+    id: 2,
+    title: "Backend Developer",
+    description: "Begin the quiz to challenge your server-side expertise!"
+  },
+  {
+    id: 3,
+    title: "Full Stack Developer",
+    description: "Take the quiz to showcase your all-round development skills!"
+  }
+]
+export const STEP_WISE_COMPONENTS = [
+  {
+    component: (props: any) => {
+       const dispatch=useAppDispatch();
+       const {selectedRole}=useAppSelector((store)=>store.commonStore)
+      return (
+        <RoleSelection
+        onOptionSelect={(role) => dispatch(updateSelectedRole(role))}
+        label="Choose Role"
+        options={Roles}
+        listIcon={true}
+        selectedOption={selectedRole}
+        {...props}
+        />
+      )
+    },
+  },
+  {
+    component: (props: any) => (
+      <TopicList  topics={SAMPLE_TOPICS} {...props}  />
+    ),
+  },
+];
 
 export default function Home() {
+   const [steps,setSteps]=useState(0)
   const [loading, setLoading] = useState(true);
+   
 
   useEffect(() => {
     // Simulate loading time
     const timer = setTimeout(() => setLoading(false), 2000); // 2 seconds
     return () => clearTimeout(timer);
   }, []);
-
+  console.log("steps",steps)
+   const StepBlockComp = STEP_WISE_COMPONENTS[steps].component
   return (
-    <main>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-        <div className="text-center mb-12">
-          <h1 className="text-5xl font-extrabold mb-4">Quiz App</h1>
-          <p className="text-xl">Select a topic to start your test</p>
+    <>
+      <div className=" w-full  grid place-content-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+        {/* <RoleSelection onOptionSelect={(role)=>console.log("all options on home page",role)} label="Choose Role" options={Roles} listIcon/>
+        <TopicList/> */}
+         <div className="container">
+         <StepBlockComp/>
+        <div className="w-full  mt-10">
+        <Pagination onNext={()=>{
+           if(steps===STEP_WISE_COMPONENTS.length-1)return
+          setSteps((prev)=>prev+1)
+          
+        }} onPrevious={()=>{
+          if(steps>=1){
+            setSteps((prev)=>prev-1)
+           }
+        }} hasNext={steps <=STEP_WISE_COMPONENTS.length-1} hasPrevious={steps>=STEP_WISE_COMPONENTS.length-1}/>
         </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 p-8">
-          {loading
-            ? Array(6) // Adjust the number based on how many skeletons you want
-                .fill(0)
-                .map((_, index) => <SkeletonPlaceholder key={index} />)
-            : SAMPLE_TOPICS.map(({ description, name, slug }) => (
-                <Link href={`/test?topic=${slug}`} legacyBehavior key={slug}>
-                  <a className="block p-6 bg-white text-gray-900 rounded-lg shadow-lg hover:scale-105 transform transition-transform duration-300">
-                    <h2 className="text-3xl font-semibold mb-2">{name}</h2>
-                    <p className="text-gray-700">{description}</p>
-                  </a>
-                </Link>
-              ))}
-        </div>
+         </div>
       </div>
-    </main>
+    </>
   );
 }
